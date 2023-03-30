dca61ce61a575e5307ef2cfe6d2642fa26fa167a
user <ermolin.andrei2017@yandex.ru>
2023-03-30 05:46:14 +0300

    wifi test

--- app/src/main/java/com/example/navigator/MainActivity.kt
+++ app/src/main/java/com/example/navigator/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.navigator
 
+import android.content.Context
+import android.net.wifi.WifiManager
+import android.net.wifi.*
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,6 +19,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+            if (!wifi.isWifiEnabled())
+                if (wifi.getWifiState() != WifiManager.WIFI_STATE_ENABLING)
+                    wifi.setWifiEnabled(true);
             NavigatorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -26,6 +33,31 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+public class WiFiAP(
+    SSID: String,
+    BSSID: String,
+    Channel: Int,
+    Level: Int,
+    Security: Int,
+    WPS: Boolean
+) {
+    private var SSID: String
+    private var BSSID: String
+    private var Channel: Int
+    private var Level: Int
+    private var Security: Int;// 0 — открытая сеть; 1 — WEP; 2 — WPA/WPA2
+    private var WPS: Boolean  // WPS?
+
+    init {
+        this.SSID = SSID;
+        this.BSSID = BSSID;
+        this.Channel = Channel;
+        this.Level = Level;
+        this.Security = Security;
+        this.WPS = WPS;
     }
 }
 
