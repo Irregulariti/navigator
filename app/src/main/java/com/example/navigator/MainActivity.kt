a56792d62c29e3d3224092c5ae5abcce00a05abf
user <ermolin.andrei2017@yandex.ru>
2023-04-14 11:21:54 +0300

    wifi scanner final

--- app/src/main/java/com/example/navigator/MainActivity.kt
+++ app/src/main/java/com/example/navigator/MainActivity.kt
@@ -1,27 +1,30 @@
 package com.example.navigator
 
 import android.Manifest
-import android.R
-import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Context.WIFI_SERVICE
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.example.navigator.ui.theme.NavigatorTheme
+import java.io.FileOutputStream
+import java.lang.Math.abs
 
 
 @Suppress("DEPRECATION")
@@ -30,95 +33,75 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                ActivityCompat.requestPermissions(
-                    activity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CHANGE_WIFI_STATE),
-                    1
-                )
-                makeEnableLocationServices(activity.applicationContext)
-            } else {
-                ActivityCompat.requestPermissions(
-                    activity,
-                    arrayOf(Manifest.permission.CHANGE_WIFI_STATE),
-                    1
-                )
-            }
+            val message = remember { mutableStateOf("") }
+            TextField(value = message.value,
+                onValueChange = { newText -> message.value = newText }, Modifier.padding(30.dp))
 
-            /* включает экран включения службы по определению местоположения */
-            fun makeEnableLocationServices(context: Context) {
-                // TODO: перед вызовом этой функции надо рассказать пользователю, зачем Вам доступ к местоположению
-                val lm: LocationManager =
-                    context.applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            Button(onClick = {
+                val wifiManager: WifiManager = getSystemService(WIFI_SERVICE) as WifiManager
 
-                val gpsEnabled: Boolean = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-                val networkEnabled: Boolean = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+                fun scanSuccess() {
+                    val results = if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        wifiManager.scanResults
+                    } else {
+                        // Permission is not granted
+                        listOf<ScanResult>()
+                    }
 
-                if (!gpsEnabled && !networkEnabled) {
-                    context.startActivity(Intent(ACTION_LOCATION_SOURCE_SETTINGS));
+                    var text = "${message.value}: \n"
+                    for (i in results) {
+                        text += i.SSID + " - "
+                        text += kotlin.math.abs(i.level).toString() + "\n"
+                    }
+                    text += "\n".repeat(3)
+                    val fos: FileOutputStream = openFileOutput("results", MODE_APPEND)
+                    fos.write(text.toByteArray())
+                    fos.close()
                 }
-            }
-            NavigatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
 
-    @SuppressLint("MissingPermission")
-    fun detectWifi() {
-        println("gffg")
-        this.wifiManager = getSystemService(WIFI_SERVICE) as WifiManager
-        this.wifiManager!!.startScan()
-        this.wifiList = this.wifiManager!!.scanResults
-        Log.d("TAG", wifiList.toString())
-        this.nets = mutableListOf()
-        for (i in 0 until wifiList!!.size) {
-            println("cikl")
-            val item: String = wifiList!!.get(i).toString()
-            val vector_item = item.split(",".toRegex()).dropLastWhile { it.isEmpty() }
-                .toTypedArray()
-            val item_essid = vector_item[0]
-            val item_capabilities = vector_item[2]
-            val item_level = vector_item[3]
-            val ssid =
-                item_essid.split(": ".toRegex()).dropLastWhile { it.isEmpty() }
-                    .toTypedArray()[1]
-            val security =
-                item_capabilities.split(": ".toRegex()).dropLastWhile { it.isEmpty() }
-                    .toTypedArray()[1]
-            val level =
-                item_level.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-            nets.add(Element(ssid, security, level))
-        }
-        println("GGF")
-        println(nets)
-        for (i in nets) {
-            println(i)
+                fun scanFailure() {
+                    // handle failure: new scan did NOT succeed
+                    // consider using old scan results: these are the OLD results!
+                    val results = wifiManager.scanResults
+                    println(results)
+                }
+
+                val success = wifiManager.startScan()
+//                val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+//                startActivity(myIntent)
+                println(success)
+                if (!success) {
+                    scanFailure()
+                }
+
+                val wifiScanReceiver = object : BroadcastReceiver() {
+
+                    override fun onReceive(context: Context, intent: Intent) {
+                        val success =
+                            intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+                        if (success) {
+                            scanSuccess()
+                        } else {
+                            scanFailure()
+                        }
+                    }
+                }
+
+                val intentFilter = IntentFilter()
+                intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+                registerReceiver(wifiScanReceiver, intentFilter)
+            }
+            ) {
+                Text("Click")
+            }
         }
     }
 }
 
-public class Element(
-    title: String,
-    security: String,
-    level: String
-) {
-    private var title: String
-    private var security: String
-    private var level: String
-
-    init {
-        this.title = title;
-        this.security = security;
-        this.level = level
-    }
-}
 
 @Composable
 fun Greeting(name: String) {
